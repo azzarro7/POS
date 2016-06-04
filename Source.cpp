5b3cfb3d2373549f0929dd7e8920df27dbf95af9
anjasz <anjasz21@gmail.com>
2016-06-04 15:44:38 +0200

    dodanie funkcji do sklejania obrazków
    Stworzyłem funkcję sklejającą obrazki, która wywoływana jest przed oraz
    po przetworzeniu. Można określić sobie w ilu wierszach i kolumnach mają
    zostać połączone obrazki oraz skalowanie rozmiaru.
    
    Musiałem pozmieniać też trochę to przetwarzanie, ponieważ obrazy
    wejściowe i wyjściowe umieściłem w wektorach. Przetwarzanie odbywa się
    teraz w pętli.

--- Source.cpp
+++ Source.cpp
@@ -6,26 +6,41 @@
 using namespace cv;
 using namespace std;
 
+Mat sklejanie(vector<Mat> in, int n);
+
 int equalize()
 {
-	Mat input, output, temp;
+	Mat temp, temp1; //temp1 - tymczasowa zmienna przechowuj¹ca obraz wyjœciowy;
 	vector<Mat> channels;
-	
+	vector<Mat> input;
+	vector<Mat> output;
 
 	char* source_window = "Obraz wejsciowy";
 	char* equalized_window = "Obraz przetworzony";
+	char* Sklejanie_przed_window = "Sklejanie_przed";
+	char* Sklejanie_po_window = "Sklejanie_po";
 
-	// ladowanie obrazu
-	input = imread("1.jpg", 1);
+	// ladowanie obrazow do wektora
+	input.push_back(imread("1.jpg", 1));
+	input.push_back(imread("2.jpg", 1));
+	int n = 2; //iloœæ wczytanych obrazow
 
-	if (!input.data)
+	if (!input[1].data)
 	{
 		cout << "Usage: ./Histogram_Demo <path_to_image>" << endl;
 		return -1;
 	}
 
+	Mat sklejanie1; //sklejenie obrazow przed przetwarzaniem
+	Mat sklejanie2; //sklejenie obrazów po przetwarzaniu
+
+	
+	sklejanie1 = sklejanie(input, n); //wywo³anie funkcji ³¹cz¹cej obrazy
+
+	for (int i = 0;i <input.size() ;i++)
+	{
 	// konwersja na format Y Cr Cb z obrazu input do temp
-	cvtColor(input, temp, CV_BGR2YCrCb);
+	cvtColor(input[i], temp, CV_BGR2YCrCb);
 
 	// rozdzielanie obrazu temp na kanaly Y Cr Cb
 	split(temp, channels);
@@ -35,17 +50,50 @@ int equalize()
 
 	// zlozenie kanalow na powrot do obrazu output
 	merge(channels, temp);
-	cvtColor(temp, output, CV_YCrCb2BGR);
-	// wyswietlanie wynikow
-	namedWindow(source_window, CV_WINDOW_AUTOSIZE);
-	namedWindow(equalized_window, CV_WINDOW_AUTOSIZE);
+	cvtColor(temp, temp1, CV_YCrCb2BGR);
 
-	imshow(source_window, input);
-	imshow(equalized_window, output);
+	output.push_back(temp1); //wpisanie przetworzonego obrazu do wektora output
+
+	}
+
+	sklejanie2 = sklejanie(output, n);
+
+	//// wyswietlanie wynikow
+	//namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+	//namedWindow(equalized_window, CV_WINDOW_AUTOSIZE);
+
+	//imshow(source_window, input);
+	//imshow(equalized_window, output);
+	imshow(Sklejanie_przed_window, sklejanie1);
+	imshow(Sklejanie_po_window, sklejanie2);
+
 
 	/// Wait until user exits the program
 	waitKey(0);
 	return 0;
+}
+
+Mat sklejanie(vector<Mat> in, int n) //in-wektor obrazów, n-iloœæ wczytanych obrazów
+{
+	//okreœlenie iloœci wierszy i kolumn
+	int wie = 1;
+	int kol = 2;
+	//obliczenie szerokoœci i wysokoœci poszczegulnych miniaturek na podztawie iliœci wierszy i kolumn
+	int s = 600. / kol;
+	int w = 0.75*s;
+
+	Mat sklej(wie*w, kol*s, CV_8UC3, CV_RGB(0, 0, 0)); //pusty obraz z czarnym t³em do sklejenia obrazow
+
+	for (int i = 0;i<in.size();i++) //pêtla sklejaj¹ca ze sob¹ miniaturki obrazów
+	{
+		//okreœlenie kolejnych punktów od których bêd¹ wstawiane miniaturki
+		int x = i%kol*s;
+		int y = i / kol*w;
+		//tworzenie miniaturek i sklejanie ich ze sob¹
+		Mat roi = sklej(Rect(x, y, s, w));
+		resize(in[i], roi, roi.size());
+	}
+	return sklej;
 }
 
 int main()
